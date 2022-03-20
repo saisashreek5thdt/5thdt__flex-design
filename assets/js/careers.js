e544f232c4c532dcead3dcfa4f656f0f4f290cc5
saiNathChary <sainath@5thdt.com>
2022-03-21 00:51:22 +0530

    added squad section

---
+++ assets/js/careers.js
@@ -0,0 +1,42 @@
+var data = [{
+        id: 1,
+        imgpath: "./assets/images/career/ui-ux1.svg"
+    },
+    {
+        id: 2,
+        imgpath: "./assets/images/career/full-stack1.svg"
+    },
+    {
+        id: 3,
+        imgpath: "./assets/images/career/react-angular1.svg"
+    },
+    {
+        id: 4,
+        imgpath: "./assets/images/career/react-angular1.svg"
+    },
+    {
+        id: 5,
+        imgpath: "./assets/images/career/animator-editor1.svg"
+    },
+    {
+        id: 6,
+        imgpath: "./assets/images/career/illustrator1.svg"
+    },
+    {
+        id: 7,
+        imgpath: "./assets/images/career/animator-editor1.svg"
+    },
+    {
+        id: 8,
+        imgpath: "./assets/images/career/mobile-dev1.svg"
+    }
+];
+let i = 0;
+showUploadform(id); {
+    let i = id;
+    let Id = data.id[i];
+    let imagePath = data.id[i].imgpath
+    if (i == Id) {
+        document.getElementById("")
+    }
+};
