d406f9ac0d235ff933e6492ae66a931ef858e084
saiNathChary <sainath@5thdt.com>
2022-05-02 13:32:12 +0530

    added changes

--- assets/js/gaming.js
+++ assets/js/gaming.js
@@ -3,11 +3,15 @@ var characters_info = [
     { imgsrc: "assets/images/arts/2d-game-character-variation/1.jpg" },
     { imgsrc: "assets/images/arts/2d-game-character-variation/3.jpg" },
     { imgsrc: "assets/images/arts/2d-game-character-variation/1.jpg" },
+    { imgsrc: "assets/images/arts/guild-master-concept/1.jpg" },
+    { imgsrc: "assets/images/arts/2d-game-character-variation/1.jpg" },
+    { imgsrc: "assets/images/arts/2d-game-character-variation/3.jpg" },
+    { imgsrc: "assets/images/arts/2d-game-character-variation/1.jpg" }
 ]
 
 var characters_section = document.getElementById("characters_row")
 
-for (let i = 0; i <= 3; i++) {
+for (let i = 0; i < characters_info.length; i++) {
     var char_row = document.createElement('div');
     char_row.className = 'char1-img';
     characters_section.appendChild(char_row);
