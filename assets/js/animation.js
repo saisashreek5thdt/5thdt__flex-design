c5f4bd219947d703f5230144f48402d99e65b137
saiNathChary <sainath@5thdt.com>
2022-05-26 14:49:34 +0530

    added changes

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -1,36 +1,28 @@
-const anim_imges_row = [
-  { imgsrc: "assets/images/arts/2d-animation/1.jpg" },
-  { imgsrc: "assets/images/arts/2d-animation/2.jpg" },
-  { imgsrc: "assets/images/arts/2d-animation/3.jpg" },
-  { imgsrc: "assets/images/arts/2d-animation/4.jpg" },
-  { imgsrc: "assets/images/arts/2d-animation/5.jpg" },
-  { imgsrc: "assets/images/arts/2d-animation/6.jpg" },
-  { imgsrc: "assets/images/arts/2d-animation/7.jpg" },
-  { imgsrc: "assets/images/arts/2d-animation/8.jpg" },
-];
-const img_3d__info = [
-  { imgsrc: "assets/images/apps/L1.png" },
-  { imgsrc: "assets/images/apps/L2.png" },
-  { imgsrc: "assets/images/apps/D2.png" },
-];
-const anim_videos = [
-  { videosrc: "assets/video/animation/1.mp4" },
-  { videosrc: "assets/video/animation/2.mp4" },
-  { videosrc: "assets/video/animation/3.mp4" },
-  { videosrc: "assets/video/animation/4.mp4" },
+// const anim_imges_row = [
+//   { imgsrc: "assets/images/arts/2d-animation/1.jpg" },
+//   { imgsrc: "assets/images/arts/2d-animation/2.jpg" },
+//   { imgsrc: "assets/images/arts/2d-animation/3.jpg" },
+//   { imgsrc: "assets/images/arts/2d-animation/4.jpg" },
+//   { imgsrc: "assets/images/arts/2d-animation/5.jpg" },
+//   { imgsrc: "assets/images/arts/2d-animation/6.jpg" },
+//   { imgsrc: "assets/images/arts/2d-animation/7.jpg" },
+//   { imgsrc: "assets/images/arts/2d-animation/8.jpg" },
+// ];
+// const img_3d__info = [
+//   { imgsrc: "assets/images/apps/L1.png" },
+//   { imgsrc: "assets/images/apps/L2.png" },
+//   { imgsrc: "assets/images/apps/D2.png" },
+// ];
+// const anim_videos = [
+//   { videosrc: "assets/video/animation/1.mp4" },
+//   { videosrc: "assets/video/animation/2.mp4" },
+//   { videosrc: "assets/video/animation/3.mp4" },
+//   { videosrc: "assets/video/animation/4.mp4" },
   // { videosrc: 'assets/video/animation/5.mp4' },
-  { videosrc: "assets/video/animation/6.mp4" },
-  { videosrc: "assets/video/animation/7.mp4" },
-  { videosrc: "assets/video/animation/8.mp4" },
-];
-
-const anim_youtube_videos = [
-  { videosrc: "https://www.youtube.com/embed/4CWfQeja8TQ" },
-  { videosrc: "https://www.youtube.com/embed/MmThIEqIub4" },
-  { videosrc: "https://www.youtube.com/embed/mBeBuZbQV6w" },
-  { videosrc: "https://www.youtube.com/embed/Jh4hBvyfSUc" },
-  { videosrc: "https://www.youtube.com/embed/2sRkD6VTYEk" },
-];
+//   { videosrc: "assets/video/animation/6.mp4" },
+//   { videosrc: "assets/video/animation/7.mp4" },
+//   { videosrc: "assets/video/animation/8.mp4" },
+// ];
 
 // const img_3d_view_info = [
 //   {
@@ -52,47 +44,14 @@ const anim_youtube_videos = [
 // ];
 
 
-// videos
-var anim_img_container = document.getElementById("animation_container");
-
-for (let i = 0; i < anim_videos.length; i++) {
-  document.getElementById("youtube_link").style.display = "block";
-  var col_block = document.createElement("div");
-  col_block.className = "col-sm-6 col-md-6 col-lg-4 col-xl-3";
-  anim_img_container.appendChild(col_block);
-
-  var anim_card_block = document.createElement("div");
-  anim_card_block.className = "anim-card";
-  col_block.appendChild(anim_card_block);
-
-  var card_block = document.createElement("div");
-  card_block.className = "card";
-  anim_card_block.appendChild(card_block);
-
-  var card_video_block = document.createElement("video");
-  // card_video_block.muted = true;
-  // card_video_block.loop = true;
-  // card_video_block.autoplay = true;
-  card_video_block.id = i;
-  card_block.appendChild(card_video_block);
-
-  var card_source_block = document.createElement("source");
-  card_source_block.src = anim_videos[i].videosrc;
-  card_source_block.type = "video/mp4";
-  card_video_block.appendChild(card_source_block);
-
-  var view_button = document.createElement("button");
-  view_button.className = "view_more";
-  view_button.textContent = "PLAY";
-  view_button.setAttribute("onclick", "loadimages(" + i + ")");
-  view_button.setAttribute("data-bs-toggle", "modal");
-  view_button.setAttribute("data-bs-target", "#exampleModalCenter");
-  card_block.appendChild(view_button);
-
-  var card_body = document.createElement("div");
-  card_body.className = "card-body";
-  card_block.appendChild(card_body);
-}
+const anim_youtube_videos = [
+  { videosrc: "https://www.youtube.com/embed/4CWfQeja8TQ" },
+  { videosrc: "https://www.youtube.com/embed/_Y7sNUddLu4" },
+  { videosrc: "https://www.youtube.com/embed/NxyhE-lR53Y" },
+  { videosrc: "https://www.youtube.com/embed/u_4f_V7Rllo" },
+  { videosrc: "https://www.youtube.com/embed/wUh7Sftopgk" },
+  { videosrc: "https://www.youtube.com/embed/tkdWBGHplGs" }
+];
 
 var anim_img_container = document.getElementById("animation_container");
 
@@ -112,8 +71,10 @@ for (let i = 0; i < anim_youtube_videos.length; i++) {
 
   var card_video_block = document.createElement("iframe");
   card_video_block.title = "YouTube video player";
+  card_video_block.id = i;
   card_video_block.src = anim_youtube_videos[i].videosrc;
   card_video_block.setAttribute("frameborder", "0");
+  card_video_block.setAttribute("volume", "0");
   card_video_block.setAttribute("allowFullScreen", "");
   card_video_block.muted = true;
   // card_video_block.setAttribute('controls', '2');
@@ -136,59 +97,44 @@ for (let i = 0; i < anim_youtube_videos.length; i++) {
   card_block.appendChild(card_video_block);
 }
 
-function loadimages(id) {
+
+function load_youtube_videos(id) {
   var Id = id;
 
   switch(Id){
     case 0:
       if(Id == 0){
-        document.getElementById("modal_video").src= anim_videos[0].videosrc;
+        document.getElementById("youtube_link").src= anim_youtube_videos[0].videosrc;
       }
        break;
     case 1:
       if(Id == 1){
-        document.getElementById("modal_video").src= anim_videos[1].videosrc;
+        document.getElementById("youtube_link").src= anim_youtube_videos[1].videosrc;
       }
       break;
     case 2:
       if(Id == 2){
-        document.getElementById("modal_video").src= anim_videos[2].videosrc;
+        document.getElementById("youtube_link").src= anim_youtube_videos[2].videosrc;
       }
       break;
     case 3:
       if(Id == 3){
-        document.getElementById("modal_video").src= anim_videos[3].videosrc;
+        document.getElementById("youtube_link").src= anim_youtube_videos[3].videosrc;
       }
        break;
     case 4:
       if(Id == 4){
-        document.getElementById("modal_video").src= anim_videos[4].videosrc;
+        document.getElementById("youtube_link").src= anim_youtube_videos[4].videosrc;
       }
       break;
     case 5:
       if(Id == 5){
-        document.getElementById("modal_video").src= anim_videos[5].videosrc;
+        document.getElementById("youtube_link").src= anim_youtube_videos[5].videosrc;
       }
        break;
-    case 6:
-      if(Id == 6){
-        document.getElementById("modal_video").src= anim_videos[6].videosrc;
-      }
-       break;
-  }
-  document.getElementById("youtube_link").style.display = "none";
-  document.getElementById("modal_video").style.display = "block";
-  // document.getElementById("img_3d").style.display = "none";
-  // document.getElementById("modal_video").src = anim_videos[id].videosrc;
-  // console.log(document.getElementById("modal_video").src);
-}
-
-function load_youtube_videos(id) {
-  // console.log(id)
+    }
   document.getElementById("youtube_link").style.display = "block";
   document.getElementById("modal_video").style.display = "none";
-  document.getElementById("youtube_link").src =
-    anim_youtube_videos[id].videosrc;
 }
 
 
@@ -196,9 +142,103 @@ function load_youtube_videos(id) {
 function pause_video() {
   //   document.getElementById("modal_img").src = "none";
   document.getElementById("youtube_link").src = "none";
-  document.getElementById("modal_video").src = "none";
+  document.getElementById("youtube_link").style.display = "none";
   //   document.getElementById("img_3d").src = "none";
   //   document.getElementById("modal_img").style.display = "none";
-  document.getElementById("youtube_link").style.display = "none";
-  document.getElementById("modal_video").style.display = "none";
+ 
+  // document.getElementById("modal_video").style.display = "none";
 }
+
+
+
+// videos
+// var anim_img_container = document.getElementById("animation_container");
+
+// for (let i = 0; i < anim_videos.length; i++) {
+//   document.getElementById("youtube_link").style.display = "block";
+//   var col_block = document.createElement("div");
+//   col_block.className = "col-sm-6 col-md-6 col-lg-4 col-xl-3";
+//   anim_img_container.appendChild(col_block);
+
+//   var anim_card_block = document.createElement("div");
+//   anim_card_block.className = "anim-card";
+//   col_block.appendChild(anim_card_block);
+
+//   var card_block = document.createElement("div");
+//   card_block.className = "card";
+//   anim_card_block.appendChild(card_block);
+
+//   var card_video_block = document.createElement("video");
+  // card_video_block.muted = true;
+  // card_video_block.loop = true;
+  // card_video_block.autoplay = true;
+//   card_video_block.id = i;
+//   card_block.appendChild(card_video_block);
+
+//   var card_source_block = document.createElement("source");
+//   card_source_block.src = anim_videos[i].videosrc;
+//   card_source_block.type = "video/mp4";
+//   card_video_block.appendChild(card_source_block);
+
+//   var view_button = document.createElement("button");
+//   view_button.className = "view_more";
+//   view_button.textContent = "PLAY";
+//   view_button.setAttribute("onclick", "loadimages(" + i + ")");
+//   view_button.setAttribute("data-bs-toggle", "modal");
+//   view_button.setAttribute("data-bs-target", "#exampleModalCenter");
+//   card_block.appendChild(view_button);
+
+//   var card_body = document.createElement("div");
+//   card_body.className = "card-body";
+//   card_block.appendChild(card_body);
+// }
+
+
+
+// function loadimages(id) {
+//   var Id = id;
+
+//   switch(Id){
+//     case 0:
+//       if(Id == 0){
+//         document.getElementById("modal_video").src= anim_videos[0].videosrc;
+//       }
+//        break;
+//     case 1:
+//       if(Id == 1){
+//         document.getElementById("modal_video").src= anim_videos[1].videosrc;
+//       }
+//       break;
+//     case 2:
+//       if(Id == 2){
+//         document.getElementById("modal_video").src= anim_videos[2].videosrc;
+//       }
+//       break;
+//     case 3:
+//       if(Id == 3){
+//         document.getElementById("modal_video").src= anim_videos[3].videosrc;
+//       }
+//        break;
+//     case 4:
+//       if(Id == 4){
+//         document.getElementById("modal_video").src= anim_videos[4].videosrc;
+//       }
+//       break;
+//     case 5:
+//       if(Id == 5){
+//         document.getElementById("modal_video").src= anim_videos[5].videosrc;
+//       }
+//        break;
+//     case 6:
+//       if(Id == 6){
+//         document.getElementById("modal_video").src= anim_videos[6].videosrc;
+//       }
+//        break;
+//   }
+//   document.getElementById("youtube_link").style.display = "none";
+//   document.getElementById("modal_video").style.display = "block";
+  // document.getElementById("img_3d").style.display = "none";
+  // document.getElementById("modal_video").src = anim_videos[id].videosrc;
+  // console.log(document.getElementById("modal_video").src);
+// }
+
